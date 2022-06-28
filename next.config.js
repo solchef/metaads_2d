e1bfd448654b0731ebbf2432f62a2be0126aa3e2
jawmeschege <jawiwy@gmail.com>
2022-06-28 15:27:06 +0400

    no modules

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-        externalDir: true,
-    }
+  reactStrictMode: true,
+  experimental: {
+    externalDir: true,
+  },
 
+  ignoreBuildErrors: true,
 }
 
 module.exports = nextConfig
