e95769cfda7be1621fd9377fbf3a6549396881f6
obaidaalam <104429303+obaidaob@users.noreply.github.com>
2022-06-28 19:03:44 +0400

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7cba439fdea15ea0"; }
+/******/ 		__webpack_require__.h = function() { return "a1d58775fec849f9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
