86a57ae254d04b4ddb971bf22cd2bd3873221939
jawmeschege <jawiwy@gmail.com>
2022-06-28 18:50:24 +0400

    accounts moving

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -52,10 +52,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script async src="http://code.jquery.com/jquery-1.9.1.js"></script>
+        <script async src="https://code.jquery.com/jquery-1.9.1.js"></script>
         <script
           async
-          src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
+          src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
         ></script>
         <script
           async
