76e808d269d03e3580d244ffc0e969e7fb5941fa
jawmeschege <jawiwy@gmail.com>
2022-06-28 15:29:15 +0400

    no modules

--- next.config.js
+++ next.config.js
@@ -5,7 +5,13 @@ const nextConfig = {
     externalDir: true,
   },
 
-  ignoreBuildErrors: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
