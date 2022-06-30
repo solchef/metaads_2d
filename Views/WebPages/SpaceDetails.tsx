2d0f18f214f614d1ee9ad1d6319155ad19c36478
mohamedaldik <mohamed@fanitrade.com>
2022-06-30 17:41:23 +0400

    spaceeeeee

--- Views/WebPages/SpaceDetails.tsx
+++ Views/WebPages/SpaceDetails.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import useCanvas from '../../hooks/useCanvas'
 
-function SpaceDetails({offcanvasRight, isCanvasRight, setIsCanvasRight }) {
+function SpaceDetails({offcanvasBottem,offcanvasLeft, isCanvasRight, setIsCanvasRight }) {
   const { squreInfo } = useCanvas()
 
   const [info, setInfo] = useState(squreInfo)
@@ -17,14 +17,14 @@ function SpaceDetails({offcanvasRight, isCanvasRight, setIsCanvasRight }) {
         data-bs-backdrop="false"
         style={{ visibility: 'visible' }}
       >
-        <div className="offcanvas-header">
-          <h3>
-            <i className="bi bi-info-circle" /> Information
+        <div className="offcanvas-header d-flex align-items-center">
+          <h3 className='d-flex align-items-center mt-1'>
+            <i className="bi bi-info-circle me-3" /> Information
           </h3>
           <button
             type="button"
             onClick={() => setIsCanvasRight(false)}
-            className="btn-close text-reset"
+            className="btn-close text-white text-reset"
             data-bs-dismiss="offcanvas"
             aria-label="Close"
           />
@@ -34,17 +34,21 @@ function SpaceDetails({offcanvasRight, isCanvasRight, setIsCanvasRight }) {
         </div>
         <div className="offcanvas-body d-flex flex-lg-column">
           <div>
-            <h3 className="mt-4">
+            <h3 className="mt-4 d-flex align-items-center">
               <i className="bi bi-geo-alt" /> {`${info.x}X ${info.y}Y`}{' '}
             </h3>
-            <h3 className="mt-4">
+            <h3 className="mt-4 d-flex align-items-center">
               <i className="bi-border" /> {info.area}{' '}
             </h3>
-            <h3 className="mt-4">
+            <h3 className="mt-4 d-flex align-items-center">
               <i className="bi bi-link-45deg text-lowercase" />
               <a
-                    onClick={offcanvasRight}
+                    onClick={ ()=>{
+                      offcanvasLeft()
 
+                      offcanvasBottem()
+                    }}
+                      href=""
                 data-bs-toggle="offcanvas"
                 data-bs-target="#offcanvasScrolling"
                 aria-controls="offcanvasScrolling"
@@ -52,12 +56,12 @@ function SpaceDetails({offcanvasRight, isCanvasRight, setIsCanvasRight }) {
                 Buy Now
               </a>
             </h3>
-            <h3 className="mt-4">
+            <h3 className="mt-4 d-flex align-items-center">
               <i className="bi bi-person" /> {info.owner}{' '}
             </h3>
             <hr />
-            <h3 className="mt-4">Status: {info.Status} </h3>
-            <h3 className="mt-4">Price: $ {info.Price} .00</h3>
+            <h3 className="mt-4 d-flex align-items-center">Status: {info.Status} </h3>
+            <h3 className="mt-4 d-flex align-items-center">Price: $ {info.Price} .00</h3>
           </div>
         </div>
       </div>
--- components/FormThree.tsx
+++ components/FormThree.tsx
@@ -7,7 +7,7 @@ const FormThree = (props) => {
 <p className="text-white">You can <i className=" bi-arrows-move " /> your plot to desired location and purchase parcels.</p>
 
 
-<form>
+<form className="btn-slide">
         <div className="input-group hoverable  ">
           <span className="input-group-text "><i className="bi-geo-alt " /></span>
           <input type="text " aria-label="x " placeholder="ENTER LOT NAME " className="form-control " />
@@ -16,16 +16,16 @@ const FormThree = (props) => {
 
    <div className="d-flex justify-content-between mt-2">
    <div className="d-flex cir">
- <div className="circlee ">
+ <div onClick={() =>props.goToStep(1)} className="circlee ">
 
  </div>
- <div className="circlee ">
+ <div onClick={() =>props.goToStep(2)} className="circlee ">
 
  </div>
- <div className="circlee active">
+ <div onClick={() =>props.goToStep(3)} className="circlee active">
 
  </div>
- <div className="circlee">
+ <div onClick={() =>props.goToStep(4)} className="circlee">
 
  </div>
 </div>
