91513a297473f9f98811370c5062ef5ec91271cc
obaidaalam <104429303+obaidaob@users.noreply.github.com>
2022-07-01 19:05:14 +0400

    fix

--- components/FormTwo.tsx
+++ components/FormTwo.tsx
@@ -3,32 +3,14 @@ import React from 'react'
 const FormTwo = (props) => {
   return (
     <>
-      <h3 className='text-white mb-4'>STEP 2 - PICK  LOT LOCATION</h3>
+      <h3 className="text-white mb-4">STEP 2 - PICK LOT LOCATION</h3>
 
-         <div className="btn-slide">
-          <p>
-          You can <i className=" bi-arrows-move " />  drag your plot to the desired location.
-
-          </p>
-            </div>
-
-            <div className="d-flex justify-content-between mt-2">
-            <div className="d-flex cir">
-          <div onClick={() =>props.goToStep(1)} className="circlee ">
-
-          </div>
-          <div onClick={() =>props.goToStep(2)} className="circlee active">
-
-          </div>
-          <div onClick={() =>props.goToStep(3)} className="circlee">
-
-          </div>
-          <div onClick={() =>props.goToStep(4)} className="circlee">
-
-          </div>
-
-        </div>
-      </form>
+      <div className="btn-slide">
+        <p>
+          You can <i className=" bi-arrows-move " /> drag your plot to the
+          desired location.
+        </p>
+      </div>
 
       <div className="d-flex justify-content-between mt-2">
         <div className="d-flex cir">
