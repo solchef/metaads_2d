083fa909372880e3cd8d2da289e55fec42c61f13
jawmeschege <jawiwy@gmail.com>
2022-07-08 01:49:31 +0400

    added metadata url

---
+++ pages/api/metadata/[quadId].tsx
@@ -0,0 +1,18 @@
+export default function handler(req, res) {
+  let x = Number(req.query.quadId) % 1000
+  let y = Math.ceil(Number(req.query.quadId) / 1000)
+
+  res.status(200).json({
+    name: 'Quad #' + req.query.quadId,
+    description:
+      'Quadspace metaverse allows businesses, meta realtors and individual NFT collectors to acquire Quad for $1. These estate can be used as space, 3d retail space or simply a place for you and your meta buddies to kick it!',
+    image:
+      'https://bafybeibaxec4sl7cbx4ey5djtofzdahowg7mv5vmfvkx3kxcfq7koecbx4.ipfs.nftstorage.link',
+    external_url: 'https://quadspace.io',
+    attributes: [
+      { trait_type: 'Row', value: x },
+      { trait_type: 'Column', value: y },
+      { trait_type: 'QuadSpace', value: req.query.quadId },
+    ],
+  })
+}
