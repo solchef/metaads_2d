b7e530ec2edb7c0b104076510228e149e13cde3c
root <root@ip-172-31-40-240.us-east-2.compute.internal>
2022-07-20 11:21:12 +0000

    resolve sell pages on production

--- Views/WebPages/PurchaseSection.tsx
+++ Views/WebPages/PurchaseSection.tsx
@@ -10,8 +10,6 @@ import { setWidth, setHeight, getLandDefSize, getLands } from './canvesGrid'
 import { About } from './About'
 import { ImageInfo } from './ImageInfo'
 import { ImageInfoButton } from './ImageInfoButton'
-import { Sellsection } from './Sellsection'
-import { Editsection } from './Editsection'
 import { Section } from './Section'
 
 function PurchaseSection({
