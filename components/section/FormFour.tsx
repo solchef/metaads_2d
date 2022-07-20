3cb02fd69c36d3065bb585bb7cc71b08315b83e4
mohamedaldik <mohamed@fanitrade.com>
2022-07-20 13:47:54 +0400

    responsive for quadspace

--- components/section/FormFour.tsx
+++ components/section/FormFour.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useWeb3Context } from '../context'
-import { useIPFS } from '../hooks/useIPFS'
-import { handleMint } from '../utils/handleMint'
-import { selectLand } from './reducers/Settings'
-import { useAppSelector } from './store/hooks'
+import { useWeb3Context } from '../../context'
+import { useIPFS } from '../../hooks/useIPFS'
+import { handleMint } from '../../utils/handleMint'
+import { selectLand } from '../reducers/Settings'
+import { useAppSelector } from '../store/hooks'
 
 const FormFour = (props) => {
   const { contracts, address } = useWeb3Context()
@@ -34,29 +34,28 @@ const FormFour = (props) => {
   }
   return (
     <>
-    
 
-      <form >
+<h3 className="text-white mb-2">SQ.NFT DATA</h3>
 
-                    <div className="input-group hoverable mb-4">
-                        <span className="input-group-text ">
-                            <i className="bi bi-upload"></i>
-                        </span>
-                        <input
-                            type="text"
-                            placeholder="Upload Image"
+      <form className="btn-slide" >
 
-                            className="form-control"
-                        />
-                    </div>
-                </form>
-                <button
-      className="btn-primary btn-slide w-100 buy-lot hoverable mt-0 "
-        onClick={() => handleSubmint()}
-      >
-        <i className="bi-cart me-2 " />
-        BUY LOT{' '}
-      </button>
+        <div className="input-group hoverable mb-2">
+          <span className="input-group-text ">
+            <i className="bi bi-upload"></i>
+          </span>
+          <input
+            type="text"
+            placeholder="Upload Image"
+
+            className="form-control"
+          />
+        </div>
+      </form>
+     
+      <p className='mt-0 pt-0'> Recommended size (100X100px png, jpg)</p>
+
+
+
       <div className="d-flex justify-content-between mt-2">
         <div className="d-flex cir">
           <div onClick={() => props.goToStep(1)} className="circlee "></div>
@@ -66,12 +65,20 @@ const FormFour = (props) => {
             onClick={() => props.goToStep(4)}
             className="circlee active"
           ></div>
+          <div onClick={() => props.goToStep(5)} className="circlee"></div>
+
         </div>
         <button
           className="btn btn-primary Back ps-5 pe-5 "
           onClick={props.previousStep}
         >
           Back
+        </button>
+        <button
+          className="btn btn-primary Next ps-5 pe-5 "
+          onClick={props.nextStep}
+        >
+          Next
         </button>
       </div>
     </>
