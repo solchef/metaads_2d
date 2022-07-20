51c378781a5c01ee99f649bccb6b8931e90a2260
jawmeschege <jawiwy@gmail.com>
2022-07-20 15:26:42 +0400

    fix top

--- pages/api/populate/index.ts
+++ pages/api/populate/index.ts
@@ -24,8 +24,8 @@ export default async function handler(req, res) {
   const quadmints = [
     [0, 0],
     [0, 750],
-    [750, 0],
-    [750, 750],
+    [800, 0],
+    [800, 750],
     [375, 400],
   ]
 
