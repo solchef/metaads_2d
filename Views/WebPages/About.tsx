ae3bc183b65e4a480fe8f393bb68e20590301749
mohamedaldik <mohamed@fanitrade.com>
2022-07-26 12:36:14 +0400

    merge old design

--- Views/WebPages/About.tsx
+++ Views/WebPages/About.tsx
@@ -6,25 +6,57 @@
 
 export const About = () => {
   return (
-   
-    <div className="offcanvas-body about pt-5  pb-5 p-0 ">
-    <h3 >ABOUT MDW </h3>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At natus assumenda dolore provident ad eaque dolorem magni quod praesentium, accusantium ipsa sit, quaerat nulla qui ipsam voluptatum tenetur dicta aspernatur?</p>
-  
-    <hr className="my-4" />
-    <h3>HOW-IT WORKS</h3>
-<div className="d-flex">
-<span className="py-1 mt-1">1</span>
-<p className="ps-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. At natus assumenda dolore provident ad eaque dolorem magni quod praesentium, accusantium ipsa sit, quaerat nulla qui ipsam voluptatum tenetur dicta aspernatur?</p>
-</div>
-<div className="d-flex">
-<span className="py-1 mt-1">2</span>
-<p className="ps-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. At natus assumenda dolore provident ad eaque dolorem magni quod praesentium, accusantium ipsa sit, quaerat nulla qui ipsam voluptatum tenetur dicta aspernatur?</p>
-</div>
-<div className="d-flex">
-<span className="py-1 mt-1">3</span>
-<p className="ps-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. At natus assumenda dolore provident ad eaque dolorem magni quod praesentium, accusantium ipsa sit, quaerat nulla qui ipsam voluptatum tenetur dicta aspernatur?</p>
-</div>
+    <div className="offcanvas-body about pt-5 p-0">
+    <h3 className="mb-4">How does it work?</h3>
+    <p>
+      Let's make history together with the Worlds largest cooperative NFT
+      project, where you can upload whatever image and URL to our 1 million
+      NFT squares on the Ethereum network. Each square is 10 X 10px and sold
+      for $1 only.
+    </p>
+
+    <div className="d-flex">
+      <p>
+        <h4>Explore (View Mode)</h4>
+        Drag with your mouse (or fingers) to explore the grid.
+      </p>
+    </div>
+    <div className="d-flex">
+      <p>
+        <h4>More Info (View Mode)</h4>
+        Tap on the grid for info on a selected parcel or NFT.
+      </p>
+    </div>
+    <div className="d-flex">
+      <p>
+        <h4>Purchase NFT (Buy Mode)</h4> Tap the Buy Now Button,
+        where you can select the NFT size, name, image and URL.
+      </p>
+    </div>
+    <div className="d-flex">
+      <p>
+        <h4>Edit your NFT (View Mode)</h4>
+        Tap on an already owned parcel to edit the image, URL or name.
+      </p>
+    </div>
+
+    <div className="d-flex">
+      <p>
+        <h4>Resell your NFT (Coming Soon)</h4> Soon, you will be
+        able to select the "For Sale on Opensea" checkbox to mark your parcel
+        block as For Sale, connecting you directly with the Opensea
+        marketplace.
+      </p>
+    </div>
+    <div>
+      <p>
+        Show off your NFTs, project creations and even your business to the
+        entire world in the digital content space. <br />
+        <br /> Share the site with your friends and followers and own a piece
+        of crypto history! ❤️ For any help or question, please contact us via
+        discord.
+      </p>
+    </div>
   </div>
   )
 }
--- Views/WebPages/Main.tsx
+++ Views/WebPages/Main.tsx
@@ -1,12 +1,23 @@
+import { selectShowMenu, setShowMenu, setViewState } from "../../components/reducers/Settings"
+import { useAppSelector ,useAppDispatch} from '../../components/store/hooks'
+
 const Main = ()=>{
+  const dispatch = useAppDispatch()
+  const showMenu = useAppSelector(selectShowMenu)
 
     return(
         <div className="offcanvas-body about pt-5   ">
                    <div className="d-flex flex-column justify-content-end align-items-end pe-2">
-                   <i className="bi bi-info-circle"></i>
-            <i className="bi bi-twitter mt-5"></i>
-            <i className="bi bi-reddit mt-5" />
-            <i className="bi bi-instagram mt-5"></i>
+                   <i 
+                    onClick={() => {
+                dispatch(setShowMenu(!showMenu))
+
+                      dispatch(setViewState(0))
+                    }}
+                   className="icon-menu bi bi-info-circle"></i>
+            <i className="icon-menu bi bi-twitter mt-5"></i>
+            <i className="icon-menu bi bi-reddit mt-5" />
+            <i className="icon-menu bi bi-instagram mt-5"></i>
           </div> 
             </div>
 
