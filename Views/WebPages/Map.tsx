8a331a81929e068fe192c7a7ea0d2491575c8dd3
obaidaalam <104429303+obaidaob@users.noreply.github.com>
2022-07-27 20:28:09 +0400

    fix error

--- Views/WebPages/Map.tsx
+++ Views/WebPages/Map.tsx
@@ -12,6 +12,7 @@ import { EffectComposer, Outline } from '@react-three/postprocessing'
 import { LoadingManager } from 'three'
 import { Loader } from '../../utils/loader'
 import { useAppSelector } from '../../components/store/hooks'
+import { vertexShader, fragmentShader } from './shaders'
 import {
   selectLand,
   selectViewState,
@@ -27,8 +28,8 @@ import {
 } from '../../components/reducers/Settings'
 import { store } from '../../components/store'
 import useSound from 'use-sound'
-import imageJson from './image.json'
 import axios from 'axios'
+// import { returnLand } from '../../utils/returnLand'
 
 let oldx, oldy
 var isMobile = false //initiate as false
@@ -54,6 +55,7 @@ export const MapView = () => {
   const [buyMode, setBuyMode] = useState(false)
   const [image, setImage] = useState('')
   const [loading, setLoading] = useState(true)
+  let pointerIsDown = false
 
   useEffect(() => {
     if (orbit.current) {
@@ -74,7 +76,6 @@ export const MapView = () => {
 
   const getImage = async () => {
     await axios.get('https://api.quadspace.io/adspsdace.json').then((data) => {
-      // console.log(data.data)
       setImage(data.data)
       setLoading(false)
     })
@@ -124,13 +125,29 @@ export const MapView = () => {
             maxZoom={1600}
             minDistance={90}
             maxDistance={1200}
+            enableDamping={false}
             mouseButtons={{ LEFT: 2, MIDDLE: 1, RIGHT: 0 }}
             onChange={() => {
-              // console.log(orbit.current)
-              // let element = orbit.current.object.matrixWorldInverse.elements
-              // if (element[12] > 500) element[12] = 500
-              // orbit.current.object.matrixWorldInverse.elements = element
-              // console.log(orbit.current.object.matrixWorldInverse.elements)
+              // if (orbit.current.object.position.x < -500) {
+              //   if (orbit.current.object.position.x != -500)
+              //     orbit.current.object.position.x = -490
+              //   orbit.current.object.updateProjectionMatrix()
+              // }
+              // if (orbit.current.object.position.x > 500) {
+              //   if (orbit.current.object.position.x != 500)
+              //     orbit.current.object.position.x = 490
+              //   orbit.current.object.updateProjectionMatrix()
+              // }
+              // if (orbit.current.object.position.z < -500) {
+              //   if (orbit.current.object.position.z != -500)
+              //     orbit.current.object.position.z = -490
+              //   orbit.current.object.updateProjectionMatrix()
+              // }
+              // if (orbit.current.object.position.z > 500) {
+              //   if (orbit.current.object.position.z != 500)
+              //     orbit.current.object.position.z = 490
+              //   orbit.current.object.updateProjectionMatrix()
+              // }
             }}
             enablePan={buyMode}
             touches={{
@@ -142,7 +159,7 @@ export const MapView = () => {
         </Suspense>
         {/* <ToolTip1 /> */}
       </group>
-      <ambientLight />
+      {/* <ambientLight /> */}
     </Canvas>
   )
 }
@@ -167,14 +184,18 @@ const GreenSquare = ({ x, y, image }) => {
     () => new TextureLoader(manager).load(image),
     []
   )
+  const texture2 = React.useMemo(
+    () => new TextureLoader(manager).load('./blank.png'),
+    []
+  )
   const [boxPosition, setBoxPosition] = useState(new Vector3(0, 0, 0))
   const [viewLand, setViewLand] = useState(false)
   const [viewBox, setViewBox] = useState(false)
   const [landPosition, setLandPosition] = useState(new Vector3(0, 0, 0))
   const [moseDown, setMouseDown] = useState(false)
   const [moseUp, setMouseUp] = useState(false)
-  const [parcels, setParcels] = useState([])
   const [moseMoved, setMouseMoved] = useState(false)
+
   const ref = useRef()
 
   const cubeRef = useRef()
@@ -191,12 +212,6 @@ const GreenSquare = ({ x, y, image }) => {
   //     console.log(cubeRef)
   //   }
   // }, [cubeRef.current])
-
-  useEffect(() => {
-    axios.get('https://quadspace.io/api/metadata/parcels').then((parc) => {
-      setParcels(parc.data.message)
-    })
-  }, [])
 
   useEffect(() => {
     let result = 0
@@ -267,10 +282,10 @@ const GreenSquare = ({ x, y, image }) => {
               )
             )
             store.dispatch(setViewState(2))
-            returnLand(
-              boxPosition.x + widthMap / 2,
-              boxPosition.z + heightMap / 2
-            )
+            // returnLand(
+            //   boxPosition.x + widthMap / 2,
+            //   boxPosition.z + heightMap / 2
+            // )
             store.dispatch(
               setLand({
                 x: boxPosition.x + widthMap / 2,
@@ -305,64 +320,6 @@ const GreenSquare = ({ x, y, image }) => {
     setMouseMoved(true)
   }
 
-  const findLand = (x1, y1, x2, y2, x, y) => {
-    if (x > x1 && x < x2 && y > y1 && y < y2) return true
-
-    return false
-  }
-
-  const returnLand = async (x, y) => {
-    let landpoint = {
-      data: false,
-      name: 'TMDW Token',
-      coords: x + ',' + y,
-      width: 1,
-      height: 1,
-      image:
-        'https://bafybeibaxec4sl7cbx4ey5djtofzdahowg7mv5vmfvkx3kxcfq7koecbx4.ipfs.nftstorage.link/',
-      status: 'Available',
-      url: '#',
-      description: `This NFT gives you full ownership of block ${
-        y * 1000 + x
-      } on TheMillionDollarWebsite.com (TMDW) It hasn't been claimed yet so click mint to buy it now!`,
-      position: y * 1000 + x,
-    }
-    store.dispatch(setViewState(2))
-    parcels.forEach((land) => {
-      if (
-        findLand(
-          land.coordX,
-          land.coordY,
-          land.coordX + land.parcelWidth,
-          land.coordX + land.parcelHeight,
-          x,
-          y
-        )
-      ) {
-        store.dispatch(setViewState(3))
-        landpoint = {
-          data: true,
-          name: land.name,
-          coords: x + ',' + y,
-          width: land.parcelWidth,
-          height: land.parcelHeight,
-          image: `https://api.quadspace.io/${land.image_temp}`,
-          status: 'booked',
-          url: land.url,
-          description: land.description
-            ? land.description
-            : `We created the Meta-Board the online version of your traditional billboard. www.TheMillionDollarWebsite.com (http://www.themilliondollarwebsite.com/) leads to the domain www.quadspace.io (http://www.quadspace.io/). Because Quadspace powers the Metaverse component of this project. Each pixel on the Meta-Board will also come with 1 parcel of land in the Quadspace metaverse as a BONUS!`,
-          position: y * 1000 + x,
-        }
-        return
-      }
-    })
-
-    store.dispatch(setParcel(landpoint))
-
-    return landpoint
-  }
-
   return (
     <>
       {loading ? (
@@ -386,7 +343,7 @@ const GreenSquare = ({ x, y, image }) => {
               setMouseMoved(true)
             }}
             onPointerOut={() => setViewBox(false)}
-            onPointerMove={({ point }) => {
+            onPointerMove={({ point, camera }) => {
               if (moseMoved) {
                 setViewBox(true)
               }
@@ -405,14 +362,24 @@ const GreenSquare = ({ x, y, image }) => {
             }}
           >
             <planeBufferGeometry args={[widthMap, heightMap]} />
-
-            <meshBasicMaterial
+            <shaderMaterial
+              uniforms={{
+                // Feed the heightmap
+                bumpTexture: { value: texture },
+                bumpTexture2: { value: texture2 },
+              }}
+              // Feed the shaders as strings
+              vertexShader={vertexShader}
+              fragmentShader={fragmentShader}
+              // side={DoubleSide}
+            />
+            {/* <meshBasicMaterial
               attach="material"
               // color={color}
               // displacementMap={texture}
               map={texture}
               side={DoubleSide}
-            />
+            /> */}
           </mesh>
           {/* {viewBox && !store.getState().settings.selectMode ? (
             <mesh position={boxPosition} ref={ref}>
