298279cfa022a09687f0aa0e75a202c869e711e5
jawmeschege <jawiwy@gmail.com>
2022-07-27 20:31:52 +0400

    final commit

--- Views/WebPages/Map.tsx
+++ Views/WebPages/Map.tsx
@@ -29,7 +29,6 @@ import {
 import { store } from '../../components/store'
 import useSound from 'use-sound'
 import axios from 'axios'
-// import { returnLand } from '../../utils/returnLand'
 
 let oldx, oldy
 var isMobile = false //initiate as false
@@ -195,7 +194,13 @@ const GreenSquare = ({ x, y, image }) => {
   const [moseDown, setMouseDown] = useState(false)
   const [moseUp, setMouseUp] = useState(false)
   const [moseMoved, setMouseMoved] = useState(false)
+  const [parcels, setParcels] = useState([])
 
+  useEffect(() => {
+    axios.get('https://quadspace.io/api/metadata/parcels').then((parc) => {
+      setParcels(parc.data.message)
+    })
+  }, [])
   const ref = useRef()
 
   const cubeRef = useRef()
@@ -282,10 +287,10 @@ const GreenSquare = ({ x, y, image }) => {
               )
             )
             store.dispatch(setViewState(2))
-            // returnLand(
-            //   boxPosition.x + widthMap / 2,
-            //   boxPosition.z + heightMap / 2
-            // )
+            returnLand(
+              boxPosition.x + widthMap / 2,
+              boxPosition.z + heightMap / 2
+            )
             store.dispatch(
               setLand({
                 x: boxPosition.x + widthMap / 2,
@@ -318,6 +323,64 @@ const GreenSquare = ({ x, y, image }) => {
     }
 
     setMouseMoved(true)
+  }
+
+  const findLand = (x1, y1, x2, y2, x, y) => {
+    if (x > x1 && x < x2 && y > y1 && y < y2) return true
+
+    return false
+  }
+
+  const returnLand = async (x, y) => {
+    let landpoint = {
+      data: false,
+      name: 'TMDW Token',
+      coords: x + ',' + y,
+      width: 1,
+      height: 1,
+      image:
+        'https://bafybeibaxec4sl7cbx4ey5djtofzdahowg7mv5vmfvkx3kxcfq7koecbx4.ipfs.nftstorage.link/',
+      status: 'Available',
+      url: '#',
+      description: `This NFT gives you full ownership of block ${
+        y * 1000 + x
+      } on TheMillionDollarWebsite.com (TMDW) It hasn't been claimed yet so click mint to buy it now!`,
+      position: y * 1000 + x,
+    }
+    store.dispatch(setViewState(2))
+    parcels.forEach((land) => {
+      if (
+        findLand(
+          land.coordX,
+          land.coordY,
+          land.coordX + land.parcelWidth,
+          land.coordX + land.parcelHeight,
+          x,
+          y
+        )
+      ) {
+        store.dispatch(setViewState(3))
+        landpoint = {
+          data: true,
+          name: land.name,
+          coords: x + ',' + y,
+          width: land.parcelWidth,
+          height: land.parcelHeight,
+          image: `https://api.quadspace.io/${land.image_temp}`,
+          status: 'booked',
+          url: land.url,
+          description: land.description
+            ? land.description
+            : `We created the Meta-Board the online version of your traditional billboard. www.TheMillionDollarWebsite.com (http://www.themilliondollarwebsite.com/) leads to the domain www.quadspace.io (http://www.quadspace.io/). Because Quadspace powers the Metaverse component of this project. Each pixel on the Meta-Board will also come with 1 parcel of land in the Quadspace metaverse as a BONUS!`,
+          position: y * 1000 + x,
+        }
+        return
+      }
+    })
+
+    store.dispatch(setParcel(landpoint))
+
+    return landpoint
   }
 
   return (
