aa996b2ba13febbc651cea590e9b450b1312df60
james chege <jawiwy@gmail.com>
2022-07-29 20:54:30 +0400

    the million dollar website

--- components/ShareSection.tsx
+++ components/ShareSection.tsx
@@ -1,16 +1,81 @@
-import {ShareSocial} from 'react-share-social' 
+import {
+    EmailShareButton,
+    FacebookShareButton,
+    HatenaShareButton,
+    InstapaperShareButton,
+    LineShareButton,
+    LinkedinShareButton,
+    LivejournalShareButton,
+    MailruShareButton,
+    OKShareButton,
+    PinterestShareButton,
+    PocketShareButton,
+    RedditShareButton,
+    TelegramShareButton,
+    TumblrShareButton,
+    TwitterShareButton,
+    ViberShareButton,
+    VKShareButton,
+    WhatsappShareButton,
+    WorkplaceShareButton
+  } from "react-share";
 
-const style = {
- background: 'transparent',
-  borderRadius: 3,
-  border: 0,
-  color: 'white',
-  padding: '0 30px',
-};
+  import {
+    EmailIcon,
+    FacebookIcon,
+    FacebookMessengerIcon,
+    HatenaIcon,
+    InstapaperIcon,
+    LineIcon,
+    LinkedinIcon,
+    LivejournalIcon,
+    MailruIcon,
+    OKIcon,
+    PinterestIcon,
+    PocketIcon,
+    RedditIcon,
+    TelegramIcon,
+    TumblrIcon,
+    TwitterIcon,
+    ViberIcon,
+    VKIcon,
+    WeiboIcon,
+    WhatsappIcon,
+    WorkplaceIcon
+  } from "react-share";
+    
+
 export default function ShareSection() {
-  return <ShareSocial 
-     style={style}
-     url ="http://www.themilliondollarwebsite.com"
-     socialTypes={['facebook','twitter','reddit']}
-   />
+  return(
+    <>
+      <FacebookShareButton
+        url={"http://www.themilliondollarwebsite.com/"}
+        quote={"THE MILLION DOLLAR WEBSITE!"}
+        hashtag={"#hashtag"}
+        title={"THE MILLION DOLLAR WEBSITE!"}
+        // description="THE MILLION DOLLAR WEBSITE!"
+        className="m-2"
+      >
+        <FacebookIcon size={32} round /> 
+      </FacebookShareButton>
+
+      <TwitterShareButton
+        title={"THE MILLION DOLLAR WEBSITE!"}
+        url={"http://www.themilliondollarwebsite.com/"}
+        hashtags={["quadspace", "themilliondollarwebsite"]}
+        className="m-2"
+      >
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
+
+      <TelegramShareButton
+        title={"THE MILLION DOLLAR WEBSITE!"}
+        url={"http://www.themilliondollarwebsite.com/"}
+        className="m-2"
+      >
+        <TelegramIcon size={32} round />
+      </TelegramShareButton>
+
+    </>
+  )
 }
