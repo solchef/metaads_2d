1a14042593b1a29177556e4214cbc0ec3b5ffb4e
solchef <jawiwy@gmail.com>
2022-08-05 22:48:06 +0400

    Update marketplace text

--- Views/WebPages/Main.tsx
+++ Views/WebPages/Main.tsx
@@ -41,6 +41,7 @@ const Main = () => {
           target="_blank"
           className="mt-3 pl-2"
           href={'https://opensea.io/' + QuadSpaceContract}
+          style={{fontSize: "10px", marginLeft:"12px"}}
         >
           <img
             className="ml-2"
