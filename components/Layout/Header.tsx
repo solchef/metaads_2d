a2b03ec2da59b89aae7ddd199c2eb5546bb1b01f
jawmeschege <jawiwy@gmail.com>
2022-08-22 19:13:42 +0400

    opensea link

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -42,8 +42,9 @@ const Header = () => {
   return (
     <>
       <nav
-        className={`navbar ${showMenu && ' active'}  sticky-top d-flex navbar-dark bg-dark  `}
-        
+        className={`navbar ${
+          showMenu && ' active'
+        }  sticky-top d-flex navbar-dark bg-dark  `}
       >
         <div className="container-fluid ">
           <div>
@@ -64,52 +65,104 @@ const Header = () => {
                   <span></span>
                   <span></span>
                 </span> */}
-                <div className='position-absolute menu-top'>
-                {!menu ? 
-          <>
-          <button onClick={()=>setMenu(!menu)} className='btn btn-primary text-start py-2  '>  <i className="bi bi-list me-2"></i> <span className='me-2'>Menu</span></button>
-          
-          
-          </>
-        :<>
+                <div className="position-absolute menu-top">
+                  {!menu ? (
+                    <>
+                      <button
+                        onClick={() => setMenu(!menu)}
+                        className="btn btn-primary text-start py-2  "
+                      >
+                        {' '}
+                        <i className="bi bi-list me-2"></i>{' '}
+                        <span className="me-2">Menu</span>
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <button
+                        onClick={() => setMenu(!menu)}
+                        className="btn text-start  btn-primary mt-2 py-2 w-100"
+                      >
+                        {' '}
+                        <i className="bi bi-x-lg me-4"></i>{' '}
+                        <span className="me-3">Menu</span>
+                      </button>{' '}
+                      <br />
+                      <button
+                        onClick={() => {
+                          dispatch(setShowMenu(true))
 
-          <button onClick={()=>setMenu(!menu)}  className='btn text-start  btn-primary mt-2 py-2 w-100'>       <i className="bi bi-x-lg me-4"></i> <span className='me-3'>Menu</span></button> <br />
-          <button   onClick={() => {
-            dispatch(setShowMenu(true))
+                          dispatch(setViewState(0))
+                        }}
+                        className="btn text-start  btn-primary mt-2 py-2 w-100"
+                      >
+                        {' '}
+                        <i className="icon-menu bi bi-info-circle me-4"></i>{' '}
+                        <span className="me-3">About</span>
+                      </button>{' '}
+                      <br />
+                      <button
+                        onClick={() => {
+                          dispatch(setShowMenu(true))
 
-            dispatch(setViewState(0))
-          }} className='btn text-start  btn-primary mt-2 py-2 w-100'>       <i className="icon-menu bi bi-info-circle me-4"  ></i> <span className='me-3'>About</span></button> <br />
-         
-          <button  onClick={() => {
-            dispatch(setShowMenu(true))
-
-            dispatch(setViewState(7))
-          }}  className='btn text-start  btn-primary mt-2 py-2 w-100'>     <i className="icon-menu bi bi-map  me-4"></i> <span className='me-3'>RoadMap</span></button> <br />
-          <a
-          className='p-0'
-          target="_blank"
-          href={'https://opensea.io/' + QuadSpaceContract}
-        > 
-          <button   className='btn text-start  btn-primary mt-2 py-2 w-100'>   <i  className="icon-menu"><img className="me-4" height="20" width="20" src="https://opensea.io/static/images/logos/opensea.svg"/></i> <span className='me-3'>MarketPlace</span></button> <br />
-         </a>
-          <a
-          className='p-0'
-          target="_blank"
-          href={'https://t.me/TheMillionDollarWebsite'}
-        >   <button   className='btn text-start  btn-primary mt-2 py-2 w-100'>   <i className="bi bi-telegram me-4"></i> <span className='me-3'>Telegram</span> </button></a> <br />
-            <a
-          className='p-0'
-          target="_blank"
-          href={'https://twitter.com/quadspaceio'}
-        >  
-         <button   className='btn text-start  btn-primary mt-2 py-2 w-100'>       <i className="bi bi-twitter me-4"></i> <span className='me-3'>Twitter</span></button> <br />
-       </a>
-
-        </>
-        }
-   
-
-        </div>
+                          dispatch(setViewState(7))
+                        }}
+                        className="btn text-start  btn-primary mt-2 py-2 w-100"
+                      >
+                        {' '}
+                        <i className="icon-menu bi bi-map  me-4"></i>{' '}
+                        <span className="me-3">RoadMap</span>
+                      </button>{' '}
+                      <br />
+                      <a
+                        className="p-0"
+                        target="_blank"
+                        href={
+                          'https://opensea.io/collection/themilliondollarwebsite'
+                        }
+                      >
+                        <button className="btn text-start  btn-primary mt-2 py-2 w-100">
+                          {' '}
+                          <i className="icon-menu">
+                            <img
+                              className="me-4"
+                              height="20"
+                              width="20"
+                              src="https://opensea.io/static/images/logos/opensea.svg"
+                            />
+                          </i>{' '}
+                          <span className="me-3">MarketPlace</span>
+                        </button>{' '}
+                        <br />
+                      </a>
+                      <a
+                        className="p-0"
+                        target="_blank"
+                        href={'https://t.me/TheMillionDollarWebsite'}
+                      >
+                        {' '}
+                        <button className="btn text-start  btn-primary mt-2 py-2 w-100">
+                          {' '}
+                          <i className="bi bi-telegram me-4"></i>{' '}
+                          <span className="me-3">Telegram</span>{' '}
+                        </button>
+                      </a>{' '}
+                      <br />
+                      <a
+                        className="p-0"
+                        target="_blank"
+                        href={'https://twitter.com/quadspaceio'}
+                      >
+                        <button className="btn text-start  btn-primary mt-2 py-2 w-100">
+                          {' '}
+                          <i className="bi bi-twitter me-4"></i>{' '}
+                          <span className="me-3">Twitter</span>
+                        </button>{' '}
+                        <br />
+                      </a>
+                    </>
+                  )}
+                </div>
                 <h3 className="pb-0 mt-1 mb-0 fs-6">
                   THE MILLION <br /> DOLLAR WEBSITE
                 </h3>
