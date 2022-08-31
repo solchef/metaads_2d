c5d3b63b1c4b0001d69785567d54aa856575ea21
jawmeschege <jawiwy@gmail.com>
2022-08-31 16:45:26 +0400

    remove dynamic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import dynamic from 'next/dynamic'
+import AdSpace from '../Views/AdSpace'
 // import { SliderSection } from '../Views/WebPages/SliderSection'
 // import { InfoSection } from '../Views/WebPages/InfoSection'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-const AdSpace = dynamic(() => import('../Views/AdSpace'), {
-  ssr: false,
-})
+// const AdSpace = dynamic(() => import('../Views/AdSpace'), {
+//   ssr: false,
+// })
 
 const Home = () => {
   return <AdSpace />
